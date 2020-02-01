09f214300a8e0a19507c59fd3a6b97b81081246a
fabiola <fabiolasilvaoffice@gmail.com>
2020-01-31 22:15:00 -0400

    Estilizando a pagina do perfil de portaria

--- portaria/index.php
+++ portaria/index.php
@@ -18,6 +18,7 @@
             <div id="porteiro">
                Seu código é:  &emsp; &emsp; &emsp; &emsp;
                <a href="../config.php?acao=quebra"> Sair </a> 
+               <img src="../img/sair.png" id="img_exit" />
             </div>  
 
             <div id="logo">
@@ -25,23 +26,27 @@
             </div>  
 
             <div id="campo_busca">
-                <form name="" method="post" action="" enctype="multpart/form-data">
-                    <input type="text" name="CPF" value="" /> 
-                    <input class="input" type="submit" name="send" value="Enviar"/>
-                </form>
 
-                <br><br><br><br><h3>Aluno:</h3> <strong>Nº de matriculas:</strong> <strong>RG:</strong> 
-                    <a href="index.php?pg=confirma&code_a =" > 
-                        <img src="../img/correto.jpg" title="Confirmar"/>
-                    </a>
+                <form name="" method="post" action="" enctype="multpart/form-data">
+                    <input type="text" id="btn_cpf" name="CPF" value="" /> 
+                    <input class="input" type="submit" name="send" id="btn_enviar" value="Enviar"/>
+                </form> <br> 
+            </div>  
+               
+
+            <div id="campo_aluno">
+                <h3>Aluno: <br>
+                    <strong>RG:</strong>  <br>
+                    <strong>Nº de matriculas:</strong> &emsp; &emsp; &emsp;
+                </h3>
                     <a href="index.php">
                         <img id="lixeira" src="../img/lixeira.png" title="Cancelar"/>
                     </a>
                     <input type="hidden" name="code" value="" />
+            </div>
 
 
-
-            </div>   
+            
 
             
         
